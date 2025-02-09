494472381f056182327b7d503d0823ea60cd579e
Floloffel <f.ulbricht@live.de>
2025-02-09 19:29:54 +0100

    implemented function

--- evaluation/Auswerteschleife.py
+++ evaluation/Auswerteschleife.py
@@ -10,107 +10,97 @@ import pyfar as pf
 import time
 
 from evaluation_config import eval_conf
+from _calc_single_audio_frame import calc_audio_frame
 
-###############################################
-initialTime = time.time()
+# define paths
+path_audio_data = "./evaluation/" + eval_conf["in_folder"] + "array_audio_data/2025-01-28_15-59-01_400437.h5"
+path_mic_geom = "./evaluation/"+  eval_conf["in_folder"] + "array_position_data/bassoon_cage_64_optimized.xml"
+path_result_file = "./evaluation/" + eval_conf["out_folder"] + "2025-01-28_15-59-01_400437_eval_test2"
 
-# initial definitions
-
-resFileName = "./evaluation/" + eval_conf["out_folder"] + "2025-01-28_15-59-01_400437_eval_test2"
-
-# define microphone geometry
-micgeofile = "./evaluation/"+  eval_conf["in_folder"] + "array_position_data/bassoon_cage_64_optimized.xml"
-m = ac.MicGeom(from_file=micgeofile)
-
-# define calculation grid
+# define parameters
 resolution = eval_conf["calc_grid_res_meters"]
+start = 0 #seconds
+stop = 105 
+bandwith = eval_conf["bandwidth"]
+frequency_bands = eval_conf["frequency_bands"]
+sample_freq = ac.MaskedTimeSamples(name = path_audio_data).sample_freq
 
-g = ac.RectGrid3D(x_min=eval_conf["x_min"], x_max=eval_conf["x_max"], y_min=eval_conf["y_min"], y_max=eval_conf["y_max"], z_min=eval_conf["z_min"], z_max=eval_conf["z_max"], increment=eval_conf["calc_grid_res_meters"])
+frame_rate = eval_conf["framereate_fps"]
+frame_length = 1/frame_rate
+frame_amount = int((stop-start)*frame_rate)
 
-# define audio data
-name = "./evaluation/" + eval_conf["in_folder"] + "array_audio_data/2025-01-28_15-59-01_400437.h5"
-data = ac.MaskedTimeSamples(name = name)
 
-# define time related variables
-start = 0#83#26        # in s
-stop = 105#35#92#36
-framerate = eval_conf["framereate_fps"]         # in f/s
 
-# define freqency resolution
-bandwith = eval_conf["bandwidth"]            #
-frequency_bands = eval_conf["frequency_bands"] #
 
-#---------------------------------------------------------------------------------------------------------------
-# calculation of fixed values
+# acoular set up
 
-frameLength = 1/framerate
-
-print('init time: ' + str(time.time()-initialTime) + ' s')
-
+m = ac.MicGeom(from_file=path_mic_geom)
+g = ac.RectGrid3D(
+    x_min=eval_conf["x_min"], 
+    x_max=eval_conf["x_max"], 
+    y_min=eval_conf["y_min"], 
+    y_max=eval_conf["y_max"], 
+    z_min=eval_conf["z_min"], 
+    z_max=eval_conf["z_max"], 
+    increment=eval_conf["calc_grid_res_meters"]
+    )
 result = np.zeros(
     [len(frequency_bands),
-     int((stop-start)*framerate),
+     frame_amount,
      g.nxsteps,
      g.nysteps,
      g.nzsteps]
      )
 
-# begin evaluation loop
+###############################################
+# start calculation
+time_initial = time.time()
 
-for i, currentFreqBand in enumerate(frequency_bands):
+for index_freq_band, currentFreqBand in enumerate(frequency_bands):
+    for index_frame in range(0, frame_amount):
+        time_calc = time.time()
 
-    for frame in range(0, int((stop-start)*framerate)):
-        calcTime = time.time()
-
-        print('Frame ' + str(frame+1)+' of '+str(int((stop-start)*framerate)))
-        print('Frequency Band: ' + str(i+1) + ' (' + str(currentFreqBand) + ' Hz)')
-        print('Frame length: ' + str((start+(frameLength*(frame+1)))-(start+(frameLength*frame))) + ' s ')
-        
-
-        # data = ac.MaskedTimeSamples(name = name)
-        data = ac.MaskedTimeSamples(name = name, start = (start+(frameLength*frame))*data.sample_freq, stop = (start+(frameLength*(frame+1)))*data.sample_freq)
-        # print('length of cropped measurement: ' + str(data.numsamples/data.sample_freq) + ' s (' + str(data.numsamples) + ' samples)')
-
-        f = ac.PowerSpectra(
-            source=data, 
-            window='Hanning', 
-            overlap=eval_conf["fft_overlap"], 
-            block_size=eval_conf["fft_block_size"]
+        result[index_freq_band, index_frame] = calc_audio_frame(
+            currentFreqBand, 
+            index_frame, 
+            index_freq_band, 
+            bandwith, 
+            start,
+            stop,
+            sample_freq,
+            frame_rate,
+            frame_length,
+            path_audio_data,
+            g,
+            m
             )
-        st = ac.SteeringVector(grid=g, mics=m, steer_type='true location')
-        b = ac.BeamformerCleansc(freq_data=f, steer=st)
-
-        result[i,frame] = b.synthetic(currentFreqBand, bandwith)
-
-        print('Calculation time:', np.round(time.time()-calcTime, 2), "seconds")
-        print("")
-
-totalCalcTime = np.round(time.time()-initialTime, 2)
+        
+time_total = np.round(time.time()-time_initial, 2)
 
 print("##############################################")
-print(f"Total calculation time: {np.round(totalCalcTime/60 , 2)} minutes")
+print(f"Total calculation time: {np.round(time_total/60 , 2)} minutes")
 print("##############################################")
 #########################################
 
 # save Data
-np.save(resFileName, result)
-print(f"Saved NumPy result to {resFileName}")
+np.save(path_result_file, result)
+print(f"Saved NumPy result to {path_result_file}")
 
-pf.io.write(resFileName, 
+pf.io.write(path_result_file, 
            start = start, 
            stop = stop, 
-           framerate = framerate, 
-           frames = int((stop-start)*framerate), 
+           framerate = frame_rate, 
+           frames = frame_amount, 
            frequency_bands = frequency_bands, 
            bandwith = bandwith,
            resolution = resolution,
-           name = name)
-print(f"Saved PyFar result to {resFileName}")
+           name = path_audio_data)
+print(f"Saved PyFar result to {path_result_file}")
 
 #####################################
 
 # read Data
 
-result = np.load(resFileName + ".npy")
-start, stop, framerate, frames, frequency_bands, bandwith, resolution, name = pf.io.read(resFileName+".far")
+result = np.load(path_result_file + ".npy")
+start, stop, framerate, frames, frequency_bands, bandwith, resolution, name = pf.io.read(path_result_file+".far")
 result_Lp = ac.L_p(result)        # convert results in sound pressure Level
