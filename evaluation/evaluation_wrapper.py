b76a3a65c9f8565a49188830ffda7081d58c7f41
Floloffel <f.ulbricht@live.de>
2025-02-10 15:00:59 +0100

    WIP Wrapper: needs specific start and stop times

--- evaluation/evaluation_wrapper.py
+++ evaluation/evaluation_wrapper.py
@@ -1,14 +1,36 @@
 import os
+import time
+import acoular
+import numpy as np
 from evaluation_config import eval_config
 from efficient_evaluation import efficient_eval
 
 
 
 path_audio_data = eval_config["in_folder"] + "array_audio_data/"
-
-print(path_audio_data)
 h5_files_list = os.listdir(path_audio_data)
 
-for file_name in h5_files_list[2:5]:
-    out_folder_name = f"result_{eval_config["framereate_fps"]}_{file_name}"
-    efficient_eval(file_name, out_folder_name, eval_config)
+
+time_start = time.time()
+
+for file_name in h5_files_list:
+    out_folder_name = f"result_{eval_config["frame_rate_fps"]}_{file_name}"
+    try:
+        efficient_eval(file_name, out_folder_name, eval_config)
+        print("###############################################")
+        print("###############################################")
+        print(file_name, "finished")
+        print("###############################################")
+        print("###############################################")
+    except:
+        print("###############################################")
+        print("###############################################")
+        print(file_name, "not successfull")
+        print("###############################################")
+        print("###############################################")
+
+
+time_needed = np.round(time.time() - time_start, 2)
+
+print(f"Calculated all files @ {eval_config["frame_rate_fps"]} fps")
+print(f"Needed time: {time_needed/60} minutes.")
