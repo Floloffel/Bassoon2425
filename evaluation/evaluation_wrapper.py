deb4de0beb09bb9e752fde694ce008737e98aa29
Floloffel <f.ulbricht@live.de>
2025-02-11 18:42:51 +0100

    working

--- evaluation/evaluation_wrapper.py
+++ evaluation/evaluation_wrapper.py
@@ -1,35 +1,33 @@
-import os
 import time
-import acoular
 import numpy as np
 from evaluation_config import eval_config
 from efficient_evaluation import efficient_eval
 
 
 
-path_audio_data = eval_config["in_folder"] + "array_audio_data/"
-h5_files_list = os.listdir(path_audio_data)
-
-start_list = [] # must be filled
-stop_list = [] # must be filled
-
 time_start = time.time()
 
-for file_name, start, stop in zip(h5_files_list, start_list, stop_list):
+# load and trim stats
+stats = np.load("./evaluation/in/audioFileStats.npy")
+stats  = stats[0:11,:]
+
+# estimator for total time needed
+total_time = []
+for row in stats:
+    time_s = float(row[3]) - float(row[2])
+    total_time.append(time_s)
+
+total_time = sum(total_time)
+estimated_time_per_seconds = 90
+estimate_time = np.round(total_time * estimated_time_per_seconds / 60 / 60, 2)
+print(f"Estimated need time with {estimated_time_per_seconds} seconds of calculation for 1 second of audio data:")
+print(f"{estimate_time} hours")
+
+
+for row in stats:
+    file_name = row[0] + ".h5"
     out_folder_name = f"result_{eval_config["frame_rate_fps"]}_{file_name}"
-    try:
-        efficient_eval(file_name, out_folder_name, eval_config)
-        print("###############################################")
-        print("###############################################")
-        print(file_name, "finished")
-        print("###############################################")
-        print("###############################################")
-    except:
-        print("###############################################")
-        print("###############################################")
-        print(file_name, "not successfull")
-        print("###############################################")
-        print("###############################################")
+    efficient_eval(file_name, out_folder_name, eval_config, float(row[2]), float(row[3]))
 
 
 time_needed = np.round(time.time() - time_start, 2)
