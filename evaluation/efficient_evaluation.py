b057b1b3d046671c124fd99208ac51535f36bde1
Floloffel <f.ulbricht@live.de>
2025-02-16 10:13:48 +0100

    trying to close .h5 file when finished + caching on none

--- evaluation/efficient_evaluation.py
+++ evaluation/efficient_evaluation.py
@@ -17,7 +17,7 @@ from evaluation_config import eval_config
 
 def efficient_eval(name_h5_file, out_folder_name, config, start_seconds, stop_seconds):
 
-    ac.config.global_caching = "individual"
+    ac.config.global_caching = "readonly"
 
     # define paths
     path_audio_data = config["in_folder"] + "array_audio_data/" + name_h5_file
@@ -127,6 +127,9 @@ def efficient_eval(name_h5_file, out_folder_name, config, start_seconds, stop_se
 
 
     print(f"Saved results to {path_result_files}")
+
+    # force closing .h5 file by setting it to None (should trigger garbage collection)
+    data = None
     #####################################
 
 def main():
